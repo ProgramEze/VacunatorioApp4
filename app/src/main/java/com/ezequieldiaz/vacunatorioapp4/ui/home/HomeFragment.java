2c2e899d8ec83767c94dc271a0bf5549326fea23
ProgramEze <programeze@gmail.com>
2025-03-14 22:09:22 -0300

    Cuarto commit

--- app/src/main/java/com/ezequieldiaz/vacunatorioapp4/ui/home/HomeFragment.java
+++ app/src/main/java/com/ezequieldiaz/vacunatorioapp4/ui/home/HomeFragment.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.ezequieldiaz.vacunatorioapp4.databinding.FragmentHomeBinding;
 
@@ -18,15 +16,8 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeFragmentViewModel homeFragmentViewModel =
-                new ViewModelProvider(this).get(HomeFragmentViewModel.class);
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        homeFragmentViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
     }
 
     @Override
